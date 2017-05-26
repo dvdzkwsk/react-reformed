92c30d7b9a49ad0de64af8a9502bd4f6945c2699
Dylan Smith <dylansmith@gmail.com>
2017-05-26 23:07:58 +0100

    merged from enhanced-validation-options

--- src/reformed.js
+++ src/reformed.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import debounce from 'debounce'
 import assign from 'object-assign'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 import getComponentName from './_internal/getComponentName'
+
+const contains = (x, xs) => xs && !!~xs.indexOf(x)
 
 const makeWrapper = (middleware) => (WrappedComponent) => {
   class FormWrapper extends React.Component {
@@ -14,7 +15,13 @@ const makeWrapper = (middleware) => (WrappedComponent) => {
       super(props, ctx)
       this.state = {
         model: props.initialModel || {},
+        lastInputEvent: {}
       }
+
+      // Store input interaction flags for the life of this
+      // component outside of state, so they can be updated
+      // during render
+      this.inputFlags = {}
     }
 
     setModel = (model) => {
@@ -28,11 +35,34 @@ const makeWrapper = (middleware) => (WrappedComponent) => {
       }))
     }
 
+    initInputFlags = (name) => {
+      if (!this.inputFlags[name]) {
+        this.setInputFlags(name, {
+          dirty: false,
+          pristine: true,
+          touched: false,
+          untouched: true
+        })
+      }
+    }
+
+    setInputFlags = (name, flags) => {
+      const merged = assign({}, this.inputFlags[name], flags)
+      const inverse = {
+        pristine: !merged.dirty,
+        untouched: !merged.touched
+      }
+
+      this.inputFlags[name] = assign({}, merged, inverse)
+      return this.inputFlags
+    }
+
     // This, of course, does not handle all possible inputs. In such cases,
     // you should just use `setProperty` or `setModel`. Or, better yet,
     // extend `reformed` to supply the bindings that match your needs.
     bindToChangeEvent = (e) => {
       const { name, type, value } = e.target
+      const newFlags = { dirty: true }
 
       if (type === 'checkbox') {
         const oldCheckboxValue = this.state.model[name] || []
@@ -40,42 +70,63 @@ const makeWrapper = (middleware) => (WrappedComponent) => {
           ? oldCheckboxValue.concat(value)
           : oldCheckboxValue.filter(v => v !== value)
 
+        newFlags.touched = true
         this.setProperty(name, newCheckboxValue)
       } else {
         this.setProperty(name, value)
       }
+
+      this.setInputFlags(name, newFlags)
     }
 
-    bindInput = (name, opts) => {
-      const cfg = assign({
-        updateOn: 'onChange',
-        debounce: 0
-      }, opts)
-
-      const { updateOn } = cfg
-      const asyncHandler = (fn) => (e) => {
-        e.persist()
-        fn(e)
-      }
-
-      const updater = (!cfg.debounce)
-        ? this.bindToChangeEvent
-        : asyncHandler(debounce(this.bindToChangeEvent, cfg.debounce, false))
-
+    bindInput = (name) => {
+      this.initInputFlags(name)
       return {
         name,
-        defaultValue: this.state.model[name] || '',
-        [updateOn]: updater
+        value: this.state.model[name] || '',
+        onChange: this.onInputEvent,
+        onBlur: this.onInputEvent,
+        onFocus: this.onInputEvent
       }
+    }
+
+    bindCheckbox = (name, value) => {
+      this.initInputFlags(name)
+      return {
+        name,
+        checked: contains(value, this.state.model[name]),
+        onChange: this.onInputEvent,
+        onBlur: this.onInputEvent,
+        onFocus: this.onInputEvent
+      }
+    }
+
+    onInputEvent = (e) => {
+      const { target, type } = e
+      const { name } = target
+      const lastInputEvent = { name, target, type }
+
+      this.setState({ lastInputEvent })
+      if (type === 'change') this.bindToChangeEvent(e)
+      if (type === 'blur') this.setInputFlags(name, { touched: true })
+    }
+
+    componentDidMount = () => {
+      // Once downstream inputs are bound and `inputFlags`
+      // has been set, force an update
+      this.forceUpdate()
     }
 
     render () {
       const nextProps = assign({}, this.props, {
         bindInput: this.bindInput,
+        bindCheckbox: this.bindCheckbox,
         bindToChangeEvent: this.bindToChangeEvent,
         model: this.state.model,
         setProperty: this.setProperty,
         setModel: this.setModel,
+        lastInputEvent: this.state.lastInputEvent,
+        inputFlags: assign({}, this.inputFlags)
       })
       // SIDE EFFECT-ABLE. Just for developer convenience and expirementation.
       const finalProps = typeof middleware === 'function'
